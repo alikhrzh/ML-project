18b19c0722d37fa4b2a58e230bd7bb9383dd0011
alikhrzh <alikhanrzh6990@gmail.com>
2026-04-22 19:00:03 +0500

    Refactor get_requirements function and authors field

--- setup.py
+++ setup.py
@@ -2,23 +2,27 @@ from setuptools import find_packages, setup
 from typing import List
 
 HYPEN_E_DOT = '-e .'
-def get_requirements(file_path:str)->List[str]:
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        [req.replace("\n", "") for req in requirements]
 
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        # Read lines and strip newlines/whitespace immediately
+        requirements = [line.strip() for line in file_obj.readlines()]
+
+        # Remove '-e .' if it exists in the list
         if HYPEN_E_DOT in requirements:
-             requirements.remove(HYPEN_E_DOT)
+            requirements.remove(HYPEN_E_DOT)
+            
     return requirements
 
 setup(
     name='mlproject',
     version='0.0.1',
-    author_1="Alikhan",
-    author_2='Nurdaulet',
+    author='Alikhan, Nurdaulet',
     author_email='alikhrzh6990@gmail.com',
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt'),
-
 )

4ef75e90cbfe5a0170b352b49520ed2d4841cfef
alikhrzh <alikhanrzh6990@gmail.com>
2026-04-23 18:52:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ def log_to_sheets(query, selected_labels, results_names):
             "recommendations": ", ".join(results_names)
         }
         response = requests.post(APPS_SCRIPT_URL, json=payload, timeout=10)
-        st.write(f"Status: {response.status_code}")  # покажет код ответа
-        st.write(f"Response: {response.text}")        # покажет ответ сервера
+        st.write(f"Status: {response.status_code}")
+        st.write(f"Response: {response.text}")
     except Exception as e:
         st.error(f"Error logging data: {e}")
 
